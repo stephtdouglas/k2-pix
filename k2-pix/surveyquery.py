b7d762d39390157e3bda99d9a200f5a286fd7a67
Ze Vinicius <jvmirca@gmail.com>
2017-12-13 10:24:48 -0800

    hack with @gully

--- k2-pix/surveyquery.py
+++ k2-pix/surveyquery.py
@@ -5,30 +5,30 @@ from astropy.io import fits
 from astropy.wcs import WCS
 from astroquery.skyview import SkyView
 import matplotlib.pyplot as plt
-import urllib2 as urllib
+import urllib
 
 # Function to Get a Survey Image Provided the Object Position & Survey Name
 # May need a downgrade of HTML5Lib: pip install --upgrade html5lib==1.0b8
 def getSVImg(SC_ObjPos, SkyViewSurvey):
-	"""
-	Input: a SkyCoord Position Vector and a Survey Type that is Compatable with NASA SkyView
-	Output: A Numpy Pixel Array and Header Array
-	"""
-	img_survey, pix_survey, hdr_survey = None, None, None
-	try:
-		img_survey = SkyView.get_images(position=SC_ObjPos, survey=[SkyViewSurvey],
-						coordinates="J2000", pixels=30)
-		if len(img_survey) > 0:
-			pix_survey = img_survey[0][0].data
-			hdr_survey = img_survey[0][0].header
-	except astroquery.exceptions.TimeoutError,urllib.HTTPError:
-		pix_survey, hdr_survey = None, None
-	return pix_survey, hdr_survey
+    """
+    Input: a SkyCoord Position Vector and a Survey Type that is Compatable with NASA SkyView
+    Output: A Numpy Pixel Array and Header Array
+    """
+    img_survey, pix_survey, hdr_survey = None, None, None
+    try:
+        img_survey = SkyView.get_images(position=SC_ObjPos, survey=[SkyViewSurvey],
+                                        coordinates="J2000", pixels=30)
+        if len(img_survey) > 0:
+            pix_survey = img_survey[0][0].data
+            hdr_survey = img_survey[0][0].header
+    except (astroquery.exceptions.TimeoutError, urllib.HTTPError):
+        pix_survey, hdr_survey = None, None
+    return pix_survey, hdr_survey
 
 if __name__ == "__main__":
-	# Demo to Showcase that the SkyView image is working.
-	Survey = 'DSS'
-	ObjPos = 'Dumbbell Nebula'
-	pixels, header = getSVImg(ObjPos, Survey)
-	plt.imshow(pixels)
-	plt.show()
+    # Demo to Showcase that the SkyView image is working.
+    Survey = 'DSS'
+    ObjPos = 'Dumbbell Nebula'
+    pixels, header = getSVImg(ObjPos, Survey)
+    plt.imshow(pixels)
+    plt.show()
